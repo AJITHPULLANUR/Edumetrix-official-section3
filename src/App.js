0d85a8dc7e8d4afadfe83f95a4eec24d971d724e
AJITHPULLANUR <ajiajith597@gmail.com>
2023-09-20 10:21:37 +0530

    section3

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import './Section3/Section3.css';
+import './Section3/Section3Media.css';
+import Demo from './Components/Demo';
+import Tester from './Components/seccard/Tester';
+import Popup from './Components/SIDE DIV/Popup';
+import Section3 from './Section3/Section3';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Section3/>
+  
+  
+    </div>
+  );
+}
+
+export default App;
